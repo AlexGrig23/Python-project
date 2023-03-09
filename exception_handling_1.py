5d6b5b55fdf286aa0718596ce793413071dcc661
AlexGrig23 <AlexGrig.Cyber@gmail.com>
2023-03-09 14:06:27 +0200

    Fixed style pep8

--- exception_handling_1.py
+++ exception_handling_1.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
-try:
-    digit = float(input("Enter digit: "))
-    conv_digit = int(digit)
-    print(conv_digit)
-except ValueError as error:
-    print(error)
+    try:
+        digit = float(input("Enter digit: "))
+        conv_digit = int(digit)
+        print(conv_digit)
+    except ValueError as error:
+        print(error)
